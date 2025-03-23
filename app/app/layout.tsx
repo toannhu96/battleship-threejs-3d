b82e54772260fd4ebac4f5b1e2da04525d9717e8
toannhu96 <toannhu.dev@gmail.com>
2025-03-23 14:50:20 +0700

    mod: add entrant href

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -13,7 +13,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <a
+          target="_blank"
+          href="https://jam.pieter.com"
+          style="font-family: 'system-ui', sans-serif; position: fixed; bottom: -1px; right: -1px; padding: 7px; font-size: 14px; font-weight: bold; background: #fff; color: #000; text-decoration: none; z-index: 10; border-top-left-radius: 12px; z-index: 10000; border: 1px solid #fff;"
+        >
+          🕹️ Vibe Jam 2025
+        </a>
+      </body>
     </html>
   );
 }
